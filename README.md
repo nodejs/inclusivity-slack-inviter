dd8ec0021298510777c1dc0891ed14b99e60e79b
Scott González <scott.gonzalez@gmail.com>
2016-04-04 19:55:24 -0400

    Initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Node.js Inclusivity Slack Inviter
+
+Automated [Slack](https://slack.com/) invitations for the [Node.js Inclusivity WG](https://github.com/nodejs/inclusivity).
+
+## Configuration
+
+Configuration settings are stored in `config.json`. The following settings must be defined:
+
+* `github`
+  * `clientId`: The client ID for the GitHub OAuth application.
+  * `secret`: The secret for the GitHub OAuth application.
+  * `minimumAge`: The minimum age, in days, that an account must exist on GitHub prior to joining the Slack team.
+* `slack`
+  * `team`: The name of the Slack team to invite users to.
+  * `token`: The token for accessing Slack's API.
+* `web`
+  * `cookieKeys`: An [array of keys for signing cookies](https://github.com/expressjs/cookie-session#keys).
+  * `port`: The port to listen on for incoming requests.
---
+++ index.js
@@ -0,0 +1,95 @@
+"use strict";
+
+const cookieSession = require("cookie-session");
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const githubAuth = require("connect-oauth-github");
+const github = require("github-request");
+const postRequest = require("request").post;
+const Slack = require("./lib/slack");
+const version = require("./package").version;
+const config = require("./config");
+
+const slackClient = new Slack(config.slack);
+
+const app = express();
+app.use(cookieSession({
+	name: "session",
+	keys: config.web.cookieKeys
+}));
+
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const gha = githubAuth.createClient({
+	id: config.github.clientId,
+	secret: config.github.secret,
+	scope: "user:email"
+});
+
+app.get("/", (request, response) => {
+	response.render("index");
+});
+
+app.get("/auth", gha.handshake);
+
+app.get("/signup", gha.authorize, (request, response) => {
+	let ghaUser = gha.users[request.sessionID];
+
+	github.request({
+		path: "/user?access_token=" + ghaUser.accessToken,
+	}, (error, data) => {
+		if (error) {
+			// TODO: Implement error page
+			return response.status(500).render("error");
+		}
+
+		// TODO: Filter on data.login blacklist
+
+		let ageInDays = (Date.now() - new Date(data.created_at)) / (1000 * 60 * 60 * 24);
+		if (ageInDays < config.github.minimumAge) {
+			// TODO: Check whitelist for approved accounts
+		}
+
+		ghaUser.githubProfile = data;
+		response.render("signup", {
+			name: data.name,
+			email: data.email
+		});
+	});
+});
+
+app.post("/signup", gha.authorize, (request, response) => {
+	let ghaUser = gha.users[request.sessionID];
+	let name = ghaUser.githubProfile.name;
+	let email = ghaUser.githubProfile.email;
+
+	slackClient.invite(email, (error, status) => {
+		if (error) {
+			if (error.code === "INVALID_EMAIL") {
+				// TODO: Let the user know
+			}
+
+			return response.status(500).render("error");
+		}
+
+		// Remove the user data to free up memory
+		delete gha.users[request.sessionID];
+
+		if (status === "ALREADY_INVITED" || status === "ALREADY_IN_TEAM") {
+			// TODO: Let the user know they're already invited or already in the team
+		}
+
+		// TODO: Log the GitHub -> email mapping
+
+		response.render("thanks", {
+			name: name,
+			email: email
+		});
+	});
+});
+
+app.listen(config.web.port, () => {
+	console.log("Node.js Inclusivity WG Slack Inviter v" + version +
+		" listening on port " + config.web.port);
+});
